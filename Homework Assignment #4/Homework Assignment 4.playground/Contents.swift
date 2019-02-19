1f8c505eef8b61653789c3872e67ce98702d1485
Patrick Kalkman <PKa@batenburgtechniek.onmicrosoft.com>
2019-02-19 10:47:21 +0100

    Added homework assignment 4 folder and playground

---
+++ Homework Assignment #4/Homework Assignment 4.playground/Contents.swift
@@ -0,0 +1,12 @@
+/*
+   Get Swifty Part 1 (Functions)
+   Homework assignment #4 for the course Get Swifty - Part 1 on https://pirple.thinkfic.com
+   Patrick Kalkman
+   patrick@simpletechture.nl
+
+   Details:
+
+*/
+
+
+
